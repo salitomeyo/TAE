744b1ff6ae10ab915b52a6155c7088b96d249e40
Jennifer Salazar <65683869+Jennifer-Salazar@users.noreply.github.com>
2021-04-01 18:23:50 -0500

    Inputs de la aplicacion shiny

--- Aplicación/App_Num_hijos/ui.R
+++ Aplicación/App_Num_hijos/ui.R
@@ -1,33 +1,61 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 shinyUI(fluidPage(
-
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
+    titlePanel("Número de hijos de los hogares  colombianos"),
+    
+    # Sidebar with a slider input 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            
+            numericInput(inputId = "Num_integrantes",
+                         label = "Ingrese el número de integrantes de su hogar",
+                         value= "1",
+                         step = 1,
+                         min=1,
+                         max =19),
+            
+            selectInput(inputId = "Estado_civil",
+                        label = "¿Cuál es el estado civil del jefe del hogar?",
+                        choices = c("No está casado(a) y vive en pareja hace menos de dos años",
+                                    "No está casado(a) y vive en pareja hace dos años o más",
+                                    "Está viudo(a)",
+                                    "Está separado(a) o divorciado(a)",
+                                    "Está soltero(a)",
+                                    "Está casado(a)Está casado(a)"),
+                        selected = c("No está casado(a) y vive en pareja hace menos de dos años")),
+            
+            radioButtons(inputId = "genero",
+                         label = "Ingrese el género del jefe del hogar",
+                         choices = c("Masculino", "Femenino"),
+                         selected = c("Masculino")),
+            
+            numericInput(inputId = "Edad",
+                         label = "Ingrese la edad del jefe del hogar",
+                         value= "48",
+                         step = 1,
+                         min = 13,
+                         max = 106),
+            
+            selectInput(inputId = "Vive_hogar_madre",
+                        label = "¿La madre del jefe del hogar vive en el hogar?",
+                        choices = c("Si", "No", "fallecida"),
+                        selected = c("Si"))
+            
         ),
-
+            
+            
+            
+        
+        
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            textOutput("Resultado")
         )
+        
     )
 ))
