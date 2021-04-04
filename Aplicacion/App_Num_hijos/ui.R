69e9880e97ef941848d63260fda78ed4d5f73651
miguealondono <malondonoci@unal.edu.co>
2021-04-03 19:24:17 -0500

    Se modifica el panel home y se convierte en sobre nosotros

--- Aplicacion/App_Num_hijos/ui.R
+++ Aplicacion/App_Num_hijos/ui.R
@@ -23,13 +23,18 @@ shinyUI(fluidPage(
                      br(),
                      br(),
                      br(),
+                     br(),
+                     br(),
                      
                      wellPanel(
                          
                          tags$h1("Bienvenido"),
                          
                          br(),
-                         br(),
+                         
+                         tags$p('Le damos gracias por hacer uso de aplicación y esperamos que la
+                              aplicación haya sido de su agrado y haga uso adecuado de la información que aquí se le brinde.'), 
+                         
                          hr(),
                          
                          fluidRow(
@@ -85,72 +90,6 @@ shinyUI(fluidPage(
         # Panel de navegación -----------------------------------------------------
         tabsetPanel(
             
-
-            # Panel de inicio ---------------------------------------------------------
-            tabPanel("Home", icon = icon("home"),
-                     
-                     tags$h1('¡Bienvenido, usuario!'),
-                     
-                     tags$p('Esta aplicación está diseñada para que usted ingrese los 
-                     datos del hogar al cual pertenece o del cual desea adquirir 
-                     información, la información requerida está enfocada principalmente
-                     en el jefe de la misma, los datos que administrara no son íntimos 
-                     o con los cuales se pueda sentir incomodo, además de que es poca 
-                     la información que proporcionara para tener la oportunidad de 
-                     obtener una predicción del número de hijos del hogar del cual 
-                     proporciono la información.'),
-                     
-                     tags$p('Le damos gracias por hacer uso de aplicación y esperamos que la 
-                     aplicación haya sido de su agrado y pueda usarla de manera 
-                     responsable y haga uso adecuado de la información que aquí 
-                     se le brinde.'),
-                     
-                     tags$p('Esta aplicacion a cual tiene la oportunidad de acceder, está 
-                     basada en un modelo de predicción Random Forest el cual fue 
-                     entrenado con un conjunto de datos de las encuestas de calidad 
-                     de vida del DANE en el año 2019 en el país de Colombia, el cual tienen 
-                     exhaustiva información de todas las características de los 
-                     hogares y en las cuales se escogieron como 
-                     más importantes las que son pedidas en la misma aplicación.'),
-                     
-                     hr(),
-                     
-                     tags$h2('Información acerca de los investigadores encargados 
-                     del ajuste del modelo de predicción y creación de la aplicación'),
-                     
-                     br(),
-                     br(),
-                     br(),
-                     
-                     tags$div(class="row row-cols-5",
-                              tags$div(class="col",
-                                 tags$img(src= "Cumple.png",class="imagen"),
-                                 tags$h5("Cristina Mercedes Ortega Benavides"),
-                                 tags$h5("Estudiante de Estadística"),
-                              ),
-                              tags$div(class="col",
-                                 tags$img(src= "Cumple.png",class="imagen"),
-                                 tags$h5("Miguel Angel Londoño Ciceros"),
-                                 tags$h5("Estudiante de Estadística"),
-                              ),
-                              tags$div(class="col",
-                                 tags$img(src= "Cumple.png",class="imagen"),
-                                 tags$h5("Jennifer Salazar Galvis"),
-                                 tags$h5("Estudiante de Estadística"),
-                              ),
-                              tags$div(class="col",
-                                 tags$img(src= "Cumple.png",class="imagen"),
-                                 tags$h5("Juan Esteban Sanchez Pulgarin"),
-                                 tags$h5("Estudiante de Estadística"),
-                              ),
-                              tags$div(class="col",
-                                 tags$img(src= "Cumple.png",class="imagen"),
-                                 tags$h5("Salome Aristizabal Giraldo"),
-                                 tags$h5("Estudiante de Ingenieria de Sistemas")
-                              ),
-                     )
-                     ),
-            
             # Panel de la app  ---------------------------------------------------------
             tabPanel("App", icon = icon("child"),
                      
@@ -158,9 +97,11 @@ shinyUI(fluidPage(
                 # División de la página verticalmente -------------------------------------
                 verticalLayout(
                     
-                    # titlePanel(
-                    #     "Número de hijos de los hogares colombianos"
-                    # ),
+                    titlePanel(
+                        
+                        tags$h2("Complete el formulario para obtener la predicción sobre el número de hijos")
+                        
+                    ),
                     
 
                     # Predicción panel condicional --------------------------------------------
@@ -274,7 +215,67 @@ shinyUI(fluidPage(
                 )
             ),
             
-
+            # Panel de inicio ---------------------------------------------------------
+            tabPanel("Sobre nosotros", icon = icon("user-edit"),
+                     
+                     tags$h1('Acerca de la aplicación'),
+                     
+                     tags$p('Esta aplicacion a cual tiene la oportunidad de acceder, está 
+                     basada en un modelo de predicción Random Forest el cual fue 
+                     entrenado con un conjunto de datos de las encuestas de calidad 
+                     de vida del DANE en el año 2019 en el país de Colombia, el cual tienen 
+                     exhaustiva información de todas las características de los 
+                     hogares y en las cuales se escogieron como 
+                     más importantes las que son pedidas en la misma aplicación.'),
+                     
+                     tags$p('Esta aplicación está diseñada para que usted ingrese los 
+                     datos del hogar al cual pertenece o del cual desea adquirir 
+                     información, la información requerida está enfocada principalmente
+                     en el jefe de la misma, los datos que administrara no son íntimos 
+                     o con los cuales se pueda sentir incomodo, además de que es poca 
+                     la información que proporcionara para tener la oportunidad de 
+                     obtener una predicción del número de hijos del hogar del cual 
+                     proporciono la información.'),
+                    
+                     
+                     hr(),
+                     
+                     tags$h2('Información acerca de los investigadores encargados 
+                     del ajuste del modelo de predicción y creación de la aplicación'),
+                     
+                     br(),
+                     br(),
+                     br(),
+                     
+                     tags$div(class="row row-cols-5",
+                              tags$div(class="col",
+                                       tags$img(src= "Cumple.png",class="imagen"),
+                                       tags$h5("Cristina Mercedes Ortega Benavides"),
+                                       tags$h5("Estudiante de Estadística"),
+                              ),
+                              tags$div(class="col",
+                                       tags$img(src= "Cumple.png",class="imagen"),
+                                       tags$h5("Miguel Angel Londoño Ciceros"),
+                                       tags$h5("Estudiante de Estadística"),
+                              ),
+                              tags$div(class="col",
+                                       tags$img(src= "Cumple.png",class="imagen"),
+                                       tags$h5("Jennifer Salazar Galvis"),
+                                       tags$h5("Estudiante de Estadística"),
+                              ),
+                              tags$div(class="col",
+                                       tags$img(src= "Cumple.png",class="imagen"),
+                                       tags$h5("Juan Esteban Sanchez Pulgarin"),
+                                       tags$h5("Estudiante de Estadística"),
+                              ),
+                              tags$div(class="col",
+                                       tags$img(src= "Cumple.png",class="imagen"),
+                                       tags$h5("Salome Aristizabal Giraldo"),
+                                       tags$h5("Estudiante de Ingenieria de Sistemas")
+                              ),
+                     )
+            ),
+            
             # Panel de video ----------------------------------------------------------
             tabPanel("Video", icon = icon("youtube")
             
